9f647692c566baa4c9c0ad213faaac78e967393a
Pickzelz <pickzelz@gmail.com>
2018-08-13 08:06:58 +0700

    - change structure Character

---
+++ Assets/src/Player/Character.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    public FICharacter control;
+    public FIWeapon weapon;
+
+    [Header("Character Status")]
+    public int Health = 100;
+
+    //private
+    private Vector3 bulletVelocity;
+    private GameManager _manager;
+
+	// Use this for initialization
+	void Start () {
+        _manager = GameManager.Instance;
+        _manager.characterObject = gameObject;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+
+        
+
+    }
+}
---
+++ Assets/src/Player/PlayerSpawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawn : MonoBehaviour {
+
+    public GameObject PlayerPrefabs;
+
+	// Use this for initialization
+	void Start () {
+        Instantiate(PlayerPrefabs, transform.position, transform.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
